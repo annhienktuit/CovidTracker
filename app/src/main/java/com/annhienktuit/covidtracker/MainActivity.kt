72201fafa8ab5a39f5f9dba1b17e6360579374da
Nhien Nguyen <annhien.se@gmail.com>
2021-09-24 15:12:00 +0700

    save instance with sharedpreferences

--- app/src/main/java/com/annhienktuit/covidtracker/MainActivity.kt
+++ app/src/main/java/com/annhienktuit/covidtracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.annhienktuit.covidtracker
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,14 +24,22 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity(), OnItemSelectedListener {
-
+    lateinit var shared:SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tvDate = findViewById<TextView>(R.id.tvDate)
         val date = getCurrentDateTime()
-        tvDate.text = date.toString("M/dd/yyyy")
         val spinner = findViewById<Spinner>(R.id.spinner)
+        tvDate.text = date.toString("M/dd/yyyy")
+        shared = getSharedPreferences("country", MODE_PRIVATE)
+        var countryName = shared.getString("countryName","nothing").toString()
+        var countryPos = shared.getInt("countryPos",1)
+        println(countryName)
+        fetchVaccination(countryName)
+        fetchData(countryName)
+        fetchTodayData(countryName)
+        spinner.setSelection(countryPos)
         spinner.onItemSelectedListener = this
     }
     private fun fetchData(country: String){
@@ -51,7 +60,8 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
                 tvRecovered.text = jsonObj.getString("recovered")
                 tvDeaths.text = jsonObj.getString("deaths")
                 val population = jsonObj.getString("population")
-                var percentage = ((tvVaccination.text.toString().toDouble()) / (population.toString().toDouble())) * 100
+                var vaxed = tvVaccination.text.toString().toDouble()
+                var percentage = (vaxed / (population.toString().toDouble())) * 100
                 percentage = roundOffDecimal(percentage)
                 tvPopulation.text = "$percentage%"
                 try {
@@ -79,11 +89,16 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
             val stringRequest = StringRequest(Request.Method.GET, url,
                 { response ->
                     println(url)
-                    var jsonObj = JSONObject(response.toString())
-                    val case = jsonObj.getString("todayCases")
-                    val death = jsonObj.getString("todayDeaths")
-                    tvTodayCases.text = "+$case"
-                    tvTodayDeaths.text = "+$death"
+                    try {
+                        var jsonObj = JSONObject(response.toString())
+                        val case = jsonObj.getString("todayCases")
+                        val death = jsonObj.getString("todayDeaths")
+                        tvTodayCases.text = "+$case"
+                        tvTodayDeaths.text = "+$death"
+                    }
+                    catch (e:Exception){
+                        println(e)
+                    }
                 },
                 { tvTodayCases.text = "That didn't work!" })
             queue.add(stringRequest)
@@ -100,25 +115,34 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
         val tvVaccination = findViewById<TextView>(R.id.tvVaccination)
         val stringRequest = StringRequest(Request.Method.GET, url,
             { response ->
-                val date = getCurrentDateTime()
-                var jsonObj = JSONObject(response.toString())
-                var vaccinationInfo = jsonObj.getJSONArray("timeline")
-                for(i in 0 until vaccinationInfo.length()){
-                    var jsonInner:JSONObject = vaccinationInfo.getJSONObject(i)
-                    tvVaccination.text = jsonInner.get("total").toString()
+                try {
+                    val date = getCurrentDateTime()
+                    var jsonObj = JSONObject(response.toString())
+                    var vaccinationInfo = jsonObj.getJSONArray("timeline")
+                    for(i in 0 until vaccinationInfo.length()){
+                        var jsonInner:JSONObject = vaccinationInfo.getJSONObject(i)
+                        tvVaccination.text = jsonInner.get("total").toString()
+                    }
                 }
+                catch (e:Exception){
+                    println(e)
+                }
+
             },
             { tvVaccination.text = "That didn't work!" })
         queue.add(stringRequest)
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        val countryName = p0!!.getItemAtPosition(p2).toString()
-        val tvTitle = findViewById<TextView>(R.id.tvTitle)
-        fetchVaccination(countryName)
-        fetchData(countryName)
-        fetchTodayData(countryName)
-        tvTitle.text = "$countryName Statistics"
+            val tvTitle = findViewById<TextView>(R.id.tvTitle)
+            val edit = shared.edit()
+            edit.putInt("countryPos",p2).apply()
+            edit.putString("countryName",p0!!.getItemAtPosition(shared.getInt("countryPos",0)).toString()).apply()
+            var countryName = shared.getString("countryName","nothing").toString()
+            fetchVaccination(countryName)
+            fetchData(countryName)
+            fetchTodayData(countryName)
+            tvTitle.text = "$countryName Statistics"
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
